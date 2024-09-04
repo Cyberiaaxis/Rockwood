245d99f5c47c3af94d035ec44b1e5144e623ef89
rockwood <cyberiaaxis@gmail.com>
2024-09-04 13:50:24 +0530

    "Removed implementation of index method in ThreadsController, leaving it empty."

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -12,10 +12,7 @@ class ThreadsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
-    }
+    public function index() {}
 
     /**
      * Show the form for creating a new resource.
